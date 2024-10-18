d5be9c4b49193c99c0013180a6b130a27444381d
tchami <131756888+tchamianest@users.noreply.github.com>
2024-10-18 20:29:55 +0200

    Feat: add the api for the basic student and their data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.*;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +18,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of( "Hello"," World");
+	public List<Student> getStudents(){
+		return List.of( 
+			new Student(
+				1L,
+				"Kalisa daniel",
+				"tchamianest@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21
+			)
+		);
 	}
 
 }
