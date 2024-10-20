e0bcd9e1fff64e2d60b126b423331021f30087b8
tchami <131756888+tchamianest@users.noreply.github.com>
2024-10-20 16:06:23 +0200

    Feat:connect to database and also make first simple API

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -7,15 +7,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents(){
-		return List.of( 
-			new Student(
-				1L,
-				"Kalisa daniel",
-				"tchamianest@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+	private final StudentRepository studentRepository;
+
+	public StudentService(StudentRepository studentRepository){
+		this.studentRepository=studentRepository;
 	}
+	public List<Student> getStudents(){
+		return studentRepository.findAll();
+	}
+
 }
